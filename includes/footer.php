7cf2d638f0cdcfbcd75bbc1be8091d25f45aea1c
Filip Michałczyk <filip_michalczyk@wp.pl>
2021-04-05 14:18:20 +0200

    finished footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,24 @@
         <footer>
-        
+            <div class="footer-container">
+                <div class="footer-links">
+                    <ul>
+                        <a href=""><li>O nas</li></a>
+                        <a href=""><li>Kontakt</li></a>
+                    </ul>
+                    <ul>
+                        <a href=""><li>Poliytka prywatności</li></a>
+                        <a href=""><li>Regulamin</li></a>
+                    </ul>
+                    <ul>
+                        <a href=""><li>FAQ</li></a>
+                        <a href=""><li>Polityka plików cookies</li></a>
+                    </ul>
+                </div>
+                
+                <div class="copyright">
+                    <p>&copy; Code1 2021</p>
+                </div>
+            </div>
         </footer>
 
     </div>
