07c893125f1e7650b805acfb3e1acda1bbeb980a
Filip Michałczyk <filip_michalczyk@wp.pl>
2021-04-07 20:03:20 +0200

    finished home page grid

--- includes/home.php
+++ includes/home.php
@@ -1,3 +1,125 @@
-<main>
-
-</main>
+<section>
+    <div class="home-page">
+        <img src="photos/home-photo.jpg">
+        <div class="recomended-courses">
+            <h1>Najpopularniejsze kursy</h1>
+            <div class="home-courses-grid">
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+            </div>
+            <h1>Najnowsze kursy</h1>
+            <div class="home-courses-grid">
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-card">
+                        <img src="photos/course-miniature.jpg" alt="">
+                        <h4>Tytuł</h4>
+                        <p>ocena</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
